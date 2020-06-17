895c2c621454f74cc1172228f4a3812254a8c56b
yohan kulasinghe <yohankulasinghe@gmail.com>
2020-06-17 10:31:15 +0530

    First commit

---
+++ CodeSamples/loadImage.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char *argv[]) {
+	Mat image;
+	image = imread("lena.jpg");
+	if (image.empty()) {
+		printf("No Image to load");
+		return -1;
+	}
+	namedWindow("My_Image",WINDOW_AUTOSIZE);
+	imshow("My_Image", image);
+	waitKey(0);
+	image.release();
+
+	return 0;
+}
